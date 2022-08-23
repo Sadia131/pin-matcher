514a64436843d8f982834fe183c7f2dfb5cc1604
Sadia131 <sadiaobaed6969@gmail.com>
2022-08-24 00:18:13 +0600

    pin matched

--- script.js
+++ script.js
@@ -9,7 +9,47 @@ function getPin(){
     }
 }
 
+//display generated button
 function generatePin(){
     const pinInput = document.getElementById('pin');
     pinInput.value = getPin();
+}
+
+//handle calculator button event
+
+const buttonContainer = document.getElementById('digit-container');
+buttonContainer.addEventListener('click', function(event){
+const digit = event.target.innerText;
+if(isNaN(digit)){
+    //handle backspace
+    //handle clear
+  if(digit == 'C'){
+    const typedInput = document.getElementById('type-pin');
+    typedInput.value = '';
+  }  
+
+}
+else{
+    const typedInput = document.getElementById('type-pin');
+    typedInput.value = typedInput.value + digit;
+}
+})
+
+function verifyPin(){
+    const pin = document.getElementById('pin').value;
+    const typedPin = document.getElementById('type-pin').value;
+    if(pin == typedPin){
+    
+        displayMatch('block', 'none');
+    }
+    else{
+        displayMatch('none', 'block');
+    }
+}
+
+function displayMatch(correctStatus, inCorrectStatus){
+    const correct = document.getElementById('correct-pin');
+        correct.style.display = correctStatus;
+      const  incorrect = document.getElementById('incorrect-pin');
+        incorrect.style.display = inCorrectStatus;
 }
